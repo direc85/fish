e2d4eccdf8259ede1f8a1b3586780734534846e6
Matti Viljanen <matti.viljanen@kapsi.fi>
2022-03-15 23:31:18 +0200

    Separate SFOS 3 and SFOS 4 builds, add build script

---
+++ fish-build.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+set -e
+FISH_NAME=fish-3.4.0-3
+
+compile() {
+  sfdk config --push target SailfishOS-${SFOS_VER}-${SFOS_ARCH} && sfdk config --push snapshot fish && sfdk build
+  mv RPMS/${FISH_NAME}.${SFOS_ARCH}.rpm ./${FISH_NAME}.${SFOS_ARCH}.${PKG_VAR}.rpm
+}
+
+# Sailfish >= 4
+SFOS_VER=4.3.0.12
+PKG_VAR=sf4
+
+rm -f rpm/fish.spec
+ln -s fish.sf4.spec.txt rpm/fish.spec
+
+SFOS_ARCH=armv7hl
+compile
+SFOS_ARCH=aarch64
+compile
+SFOS_ARCH=i486
+compile
+
+# Sailfish < 4
+SFOS_VER=3.4.0.24
+PKG_VAR=sf3
+
+rm -f rpm/fish.spec
+ln -s fish.sf3.spec.txt rpm/fish.spec
+
+SFOS_ARCH=armv7hl
+compile
+SFOS_ARCH=i486
+compile
